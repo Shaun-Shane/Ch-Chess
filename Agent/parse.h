b84ecb06615f33067148a65e8f5c5c976a8ac550
Shaun-Shane <1953181@tongji.edu.cn>
2021-04-30 20:13:02 +0800

    Update parse.h

--- Agent/parse.h
+++ Agent/parse.h
@@ -1,69 +1,63 @@
-#define _CRT_SECURE_NO_WARNINGS
+#include <shlwapi.h>
 #include <stdio.h>
 #include <string.h>
 #include <windows.h>
-#include <shlwapi.h>
-#include<algorithm>
+
+#include <algorithm>
 
 inline char *strcasestr(const char *sz1, const char *sz2) {
-  return StrStrIA(sz1, sz2);
+    return StrStrIA(sz1, sz2);
 }
 
 inline bool streqv(const char *sz1, const char *sz2) {
-  return strncasecmp(sz1, sz2, strlen(sz2)) == 0;
+    return strncasecmp(sz1, sz2, strlen(sz2)) == 0;
 }
 
 inline bool streqvskip(const char *&sz1, const char *sz2) {
-  if (strncasecmp(sz1, sz2, strlen(sz2)) == 0) {
-    sz1 += strlen(sz2);
-    return true;
-  } 
-  else {
-    return false;
-  }
+    if (strncasecmp(sz1, sz2, strlen(sz2)) == 0) {
+        sz1 += strlen(sz2);
+        return true;
+    } else {
+        return false;
+    }
 }
 
 inline bool streqvskip(char *&sz1, const char *sz2) {
-  if (strncasecmp(sz1, sz2, strlen(sz2)) == 0) {
-    sz1 += strlen(sz2);
-    return true;
-  } else {
-    return false;
-  }
+    if (strncasecmp(sz1, sz2, strlen(sz2)) == 0) {
+        sz1 += strlen(sz2);
+        return true;
+    } else {
+        return false;
+    }
 }
 
 inline bool strscanskip(const char *&sz1, const char *sz2) {
-  const char *lpsz;
-  lpsz = strcasestr(sz1, sz2);
-  if (lpsz == NULL) {
-    return false;
-  } else {
-    sz1 = lpsz + strlen(sz2);
-    return true;
-  }
+    const char *lpsz;
+    lpsz = strcasestr(sz1, sz2);
+    if (lpsz == NULL) {
+        return false;
+    } else {
+        sz1 = lpsz + strlen(sz2);
+        return true;
+    }
 }
 
 inline bool strscanskip(char *&sz1, const char *sz2) {
-  char *lpsz;
-  lpsz = strcasestr(sz1, sz2);
-  if (lpsz == NULL) {
-    return false;
-  } else {
-    sz1 = lpsz + strlen(sz2);
-    return true;
-  }
+    char *lpsz;
+    lpsz = strcasestr(sz1, sz2);
+    if (lpsz == NULL) {
+        return false;
+    } else {
+        sz1 = lpsz + strlen(sz2);
+        return true;
+    }
 }
 
-inline int str2digit(const char *sz, int nMin, int nMax) 
-{
-  int nRet;
-  if (sscanf(sz, "%d", &nRet) > 0) 
-  {
-    return std::min(std::max(nRet, nMin), nMax);
-  } 
-  else 
-  {
-    return nMin;
-  }
+inline int str2digit(const char *sz, int nMin, int nMax) {
+    int nRet;
+    if (sscanf(sz, "%d", &nRet) > 0) {
+        return std::min(std::max(nRet, nMin), nMax);
+    } else {
+        return nMin;
+    }
 }
-
