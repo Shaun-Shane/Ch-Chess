818702322c3f02f2311388933a54c38d55d6da20
andrewiner <3275284157@qq.com>
2021-05-22 12:11:30 +0800

    Update parse.h

--- Agent/parse.h
+++ Agent/parse.h
@@ -9,11 +9,11 @@ inline char *strcasestr(const char *str1, const char *str2) {
     return StrStrIA(str1, str2);
 }
 
-inline bool streqv(const char *str1, const char *str2) {
+inline bool strequal(const char *str1, const char *str2) {
     return strncasecmp(str1, str2, strlen(str2)) == 0;
 }
 
-inline bool streqvskip(const char *&str1, const char *str2) {
+inline bool strequalskip(const char *&str1, const char *str2) {
     if (strncasecmp(str1, str2, strlen(str2)) == 0) {
         str1 += strlen(str2);
         return true;
@@ -22,7 +22,7 @@ inline bool streqvskip(const char *&str1, const char *str2) {
     }
 }
 
-inline bool streqvskip(char *&str1, const char *str2) {
+inline bool strequalskip(char *&str1, const char *str2) {
     if (strncasecmp(str1, str2, strlen(str2)) == 0) {
         str1 += strlen(str2);
         return true;
@@ -32,23 +32,23 @@ inline bool streqvskip(char *&str1, const char *str2) {
 }
 
 inline bool strscanskip(const char *&str1, const char *str2) {
-    const char *strPointer;
-    strPointer = strcasestr(str1, str2);
-    if (strPointer == NULL) {
+    const char *strPtr;
+    strPtr = strcasestr(str1, str2);
+    if (strPtr == NULL) {
         return false;
     } else {
-        str1 = strPointer + strlen(str2);
+        str1 = strPtr + strlen(str2);
         return true;
     }
 }
 
 inline bool strscanskip(char *&str1, const char *str2) {
-    char *strPointer;
-    strPointer = strcasestr(str1, str2);
-    if (strPointer == NULL) {
+    char *strPtr;
+    strPtr = strcasestr(str1, str2);
+    if (strPtr == NULL) {
         return false;
     } else {
-        str1 = strPointer + strlen(str2);
+        str1 = strPtr + strlen(str2);
         return true;
     }
 }
